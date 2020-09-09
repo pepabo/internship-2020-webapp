078c6b9bd601d865b5248ad4df5be5904c3f865b
Shogen Ikoma <ikoshomail@gmail.com>
2020-09-09 15:57:04 +0900

    add blog box component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import { HelloBox } from '~/components/hello-box'
+import { BlogBox } from '~/components/blog-box'
 
 export const IndexPage = () => (
+
   <div>
+
     <h1>BASE APPLICATION</h1>
+
+
     <HelloBox />
+    <BlogBox />
   </div>
 )
 export default IndexPage
