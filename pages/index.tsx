bad9dcafb255ed0bdb93de718a1504e6c9d49f55
dojineko <dojineko.spiral@gmail.com>
2020-09-10 14:48:02 +0900

    メニューをちょっとおしゃれに

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,30 @@
-import { HelloBox } from '~/components/hello-box'
-import { BlogBox } from '~/components/blog-box'
-import { LoginBox } from '~/components/login-box'
-import Link from "next/link"
+import Link from 'next/link'
 
 export const IndexPage = () => (
   <div>
     <h1>BASE APPLICATION</h1>
-    <Link href="/abc/blog"><a>test</a></Link>
-    <br />
-    <Link href="/loginpage"><a>ログイン</a></Link>
-    <br />
-    <Link href="/blogList"><a>ブログ一覧</a></Link>
-    <br />
-    <Link href="/blogPost"><a>記事を投稿</a></Link>
-  </div >
+    <ul>
+      <li>
+        <Link href="/signup">
+          <a>🌄 サインアップ</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/login">
+          <a>🏖 ログイン</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/articles">
+          <a>🌻 ブログ一覧</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/artilces/new">
+          <a>📝 記事を投稿</a>
+        </Link>
+      </li>
+    </ul>
+  </div>
 )
 export default IndexPage
