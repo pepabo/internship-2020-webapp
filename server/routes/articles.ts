57358cbb782fc7a0f66ea20f48a27b25a5d6131a
waric0 <22999833+waric0@users.noreply.github.com>
2020-09-10 15:52:31 +0900

    記事を本人だけが編集できるように

--- server/routes/articles.ts
+++ server/routes/articles.ts
@@ -52,6 +52,60 @@ articlesRouter.get(
   }),
 )
 
+// 編集する記事を取得
+articlesRouter.get(
+  '/request/:id',
+  wrap(async (req, res) => {
+    const id: string = req.params.id || ''
+    const userId: string = req.userId || ''
+
+    if (!id || !userId) {
+      res.sendStatus(400)
+      return
+    }
+
+    const mgr = getManager()
+    const result = await mgr.findOne(ArticleEntity, { id, userId })
+
+    if (!result) {
+      res.sendStatus(404)
+      return
+    }
+
+    res.status(200).json(result)
+  }),
+)
+
+//記事の内容を上書き
+articlesRouter.post(
+  '/update/:id',
+  wrap(async (req, res) => {
+    const id: string = req.params.id || ''
+    const userId: string = req.userId || ''
+    const title: string = req.body.title || ''
+    const body: string = req.body.body || ''
+
+    if (!id || !title || !body || !userId) {
+      res.sendStatus(400)
+      return
+    }
+
+    const mgr = getManager()
+    const article = await mgr.findOne(ArticleEntity, { id, userId })
+
+    if (!article) {
+      res.sendStatus(404)
+      return
+    }
+
+    article.title = title
+    article.body = body
+    const result = await mgr.save(article)
+
+    res.status(200).json(result)
+  }),
+)
+
 articlesRouter.delete(
   '/:id',
   wrap(async (req, res) => {
