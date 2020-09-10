6b84847852b15c70c2b477b82e7708c2fd5adb3d
Shogen Ikoma <ikoshomail@gmail.com>
2020-09-10 11:48:53 +0900

    change typeorm

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import { HelloBox } from '~/components/hello-box'
 import { BlogBox } from '~/components/blog-box'
-<<<<<<< HEAD
-=======
 import { LoginBox } from '~/components/login-box'
->>>>>>> 280133b436aebe4916deaeebacdafdbf96d2452d
 
 export const IndexPage = () => (
 
@@ -11,12 +8,7 @@ export const IndexPage = () => (
 
     <h1>BASE APPLICATION</h1>
 
-<<<<<<< HEAD
-
-    <HelloBox />
-=======
     <LoginBox />
->>>>>>> 280133b436aebe4916deaeebacdafdbf96d2452d
     <BlogBox />
   </div>
 )
